5aa3386cf24c7b3b1fdd29f70106d3cbc569152f
CicloSuperior <CicloSuperior>
2017-03-21 19:57:20 +0100

    welcome botones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,14 +14,14 @@
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-6">
-          <h2>Heading</h2>
+          <h3>Nuevas y mejoradas funcionalidades</h3>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-primary" href="#" role="button">Ver más &raquo;</a></p>
         </div>
         <div class="col-md-6">
-          <h2>Heading</h2>
+          <h3>Crea tus notas personalizadas</h3>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-primary" href="#" role="button">Ver más &raquo;</a></p>
        </div>
        
       </div>
